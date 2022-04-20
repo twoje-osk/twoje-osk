fcc65d21a4f5efccd96a9daa18a0a67443284f7f
Bartosz Legięć <bartosz@legiec.io>
2022-04-20 18:00:11 +0200

    Finish setting up frontend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/* eslint-disable import/no-extraneous-dependencies */
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+const PROXY_HOST = process.env.PROXY_HOST ?? 'localhost:8080';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/api': `http://${PROXY_HOST}`,
+    },
+  },
+});
--- shared/src/dto/user/user.dto.ts
+++ shared/src/dto/user/user.dto.ts
@@ -1,10 +1,13 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsNumberString, IsString } from 'class-validator';
 
 export class UserTestDto {
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   a: string;
 
+  @ApiProperty()
   @IsNumberString()
   @IsNotEmpty()
   b: number;
