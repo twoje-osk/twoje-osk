ef9a651f8652f00e0198d74c3dd5ecc4285e9f7e
Bartosz Legięć <bartosz@legiec.io>
2022-05-17 01:27:00 +0200

    Add custom eslint rule, add require-decorator rule (for ApiResponse) (#25)
    * Add custom eslint rule, add require-decorator rule (for ApiResponse)
    
    * Adjust dockerfiles

--- backend/src/debug/debug.module.ts
+++ backend/src/debug/debug.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
-import { DebugService } from './debug.service';
 import { DebugController } from './debug.controller';
 
 @Module({
   controllers: [DebugController],
-  providers: [DebugService],
 })
 export class DebugModule {}
