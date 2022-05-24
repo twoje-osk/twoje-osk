5d7f849366c4ff1931d1683932223d45cbdd8f0a
vvooycik <maciek.wojcik22@gmail.com>
2022-05-24 23:28:41 +0200

    Adjust list view for instructors list so that it matched the trainees list

--- frontend/src/views/Instructors/InstructorsList/InstructorsList.tsx
+++ frontend/src/views/Instructors/InstructorsList/InstructorsList.tsx
@@ -1,13 +1,17 @@
 import {
-  Box,
   Typography,
   Table,
   TableHead,
   TableRow,
   TableCell,
   TableBody,
+  Toolbar,
+  Tooltip,
+  TableContainer,
+  Icon,
+  IconButton,
 } from '@mui/material';
-// import { useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { Flex } from 'reflexbox';
 import useSWR from 'swr';
 import { InstructorFindAllResponseDto } from '@osk/shared';
@@ -17,7 +21,7 @@ import { GeneralAPIError } from '../../../components/GeneralAPIError/GeneralAPIE
 export const InstructorsList = () => {
   const { data, error } =
     useSWR<InstructorFindAllResponseDto>('/api/instructors');
-  // const navigate = useNavigate();
+  const navigate = useNavigate();
   const pageTitle = 'Instruktorzy';
 
   if (error) {
@@ -31,37 +35,51 @@ export const InstructorsList = () => {
   const rows = data.instructors;
 
   return (
-    <Box p="2rem">
-      <Typography variant="h5" component="h1">
-        {pageTitle}
-      </Typography>
-      <Flex justifyContent="center" mb="1.5rem">
-        <Table>
+    <Flex flexDirection="column" height="100%">
+      <Toolbar
+        sx={{
+          pl: { sm: 2 },
+          pr: { xs: 1, sm: 1 },
+        }}
+      >
+        <Typography sx={{ flex: '1 1 100%' }} variant="h6" component="h1">
+          {pageTitle}
+        </Typography>
+        <Tooltip title="Filtruj listę">
+          <IconButton>
+            <Icon>filter_list</Icon>
+          </IconButton>
+        </Tooltip>
+      </Toolbar>
+      <TableContainer sx={{ flex: '1 1', overflow: 'auto' }}>
+        <Table aria-label="Lista Instruktorów" stickyHeader>
           <TableHead>
             <TableRow>
-              <TableCell>Imię i nazwisko</TableCell>
-              <TableCell align="right">Email</TableCell>
-              <TableCell align="right">Telefon</TableCell>
-              <TableCell align="right">Samochód</TableCell>
+              <TableCell>Imię i Nazwisko</TableCell>
+              <TableCell>Email</TableCell>
+              <TableCell>Telefon</TableCell>
+              <TableCell>Samochód</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
             {rows.map((row) => (
               <TableRow
+                hover
                 key={row.id}
-                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                onClick={() => navigate(`./${row.id}`)}
+                sx={{ cursor: 'pointer' }}
               >
                 <TableCell component="th" scope="row">
                   {`${row.user.firstName} ${row.user.lastName}`}
                 </TableCell>
-                <TableCell align="right">{row.user.email}</TableCell>
-                <TableCell align="right">{row.user.phoneNumber}</TableCell>
-                <TableCell align="right" />
+                <TableCell>{row.user.email}</TableCell>
+                <TableCell>{row.user.phoneNumber}</TableCell>
+                <TableCell />
               </TableRow>
             ))}
           </TableBody>
         </Table>
-      </Flex>
-    </Box>
+      </TableContainer>
+    </Flex>
   );
 };
