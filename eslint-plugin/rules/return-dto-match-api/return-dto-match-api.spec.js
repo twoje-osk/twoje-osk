6960ff4ed5f2c081cc9d83e9c1bc0c89e59c8a48
Bartosz Legięć <bartosz@legiec.io>
2022-05-29 00:51:03 +0200

    Adjust Custom ESLint rules (#31)

--- eslint-plugin/rules/return-dto-match-api/return-dto-match-api.spec.js
+++ eslint-plugin/rules/return-dto-match-api/return-dto-match-api.spec.js
@@ -1,7 +1,7 @@
 const { RuleTester } = require('eslint');
 const returnDtoMatchApiRule = require('./return-dto-match-api.js');
 const ruleTester = new RuleTester({
-  parser: require.resolve('@typescript-eslint/parser')
+  parser: require.resolve('@typescript-eslint/parser'),
 });
 
 ruleTester.run('return-dto-match-api', returnDtoMatchApiRule, {
@@ -68,17 +68,6 @@ ruleTester.run('return-dto-match-api', returnDtoMatchApiRule, {
             type: OtherDto
           })
           async asyncIncorrect(): Promise<Dto> {}
-        }
-      `,
-      errors: [{ messageId: 'typeDoesntMatch' }],
-    },
-    {
-      code: `
-        class Controller {
-          @ApiResponse({
-            type: OtherDto
-          })
-          async asyncIncorrect(): OtherDto {}
         }
       `,
       errors: [{ messageId: 'typeDoesntMatch' }],
