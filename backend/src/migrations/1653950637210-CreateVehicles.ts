e34f5c594939c6d430c7c7e2c944b0f8ed9bd87f
maonat <romeo.morcia@gmail.com>
2022-05-31 00:50:45 +0200

    Updated seed and CreateVehicles migration file

--- backend/src/migrations/1653950637210-CreateVehicles.ts
+++ backend/src/migrations/1653950637210-CreateVehicles.ts
@@ -1,10 +1,10 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class CreateVehicles1653932337420 implements MigrationInterface {
-    name = 'CreateVehicles1653932337420'
+export class CreateVehicles1653950637210 implements MigrationInterface {
+    name = 'CreateVehicles1653950637210'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TABLE "vehicle" ("id" SERIAL NOT NULL, "licensePlate" character varying NOT NULL, "notes" character varying NOT NULL, "organizationId" integer, CONSTRAINT "UQ_a654a0355ae4c5ba31c5f6c8925" UNIQUE ("licensePlate"), CONSTRAINT "PK_187fa17ba39d367e5604b3d1ec9" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE TABLE "vehicle" ("id" SERIAL NOT NULL, "name" character varying NOT NULL, "licensePlate" character varying NOT NULL, "vin" character varying NOT NULL, "dateOfNextCheck" TIMESTAMP NOT NULL, "photo" text, "additionalDetails" text, "notes" text, "organizationId" integer, CONSTRAINT "PK_187fa17ba39d367e5604b3d1ec9" PRIMARY KEY ("id"))`);
         await queryRunner.query(`ALTER TABLE "vehicle" ADD CONSTRAINT "FK_dd963173bbe67e11aadc6615543" FOREIGN KEY ("organizationId") REFERENCES "organization"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
     }
 
--- backend/src/seeds/seed.vehicles.ts
+++ backend/src/seeds/seed.vehicles.ts
@@ -9,11 +9,23 @@ class VehicleFactory extends Factory<Vehicle> {
 
   public generate() {
     const vehicle = new Vehicle();
+    vehicle.name = `${this.faker.vehicle.manufacturer()} ${this.faker.vehicle.model()}`;
     vehicle.licensePlate = this.faker.vehicle.vrm();
+    vehicle.vin = this.faker.vehicle.vin();
+    vehicle.dateOfNextCheck = this.faker.date.soon(300);
+    vehicle.photo =
+      this.faker.datatype.number({ max: 1, min: 0, precision: 0.0000001 }) < 0.5
+        ? this.faker.image.avatar()
+        : null;
+    vehicle.additionalDetails =
+      this.faker.datatype.number({ max: 1, min: 0, precision: 0.0000001 }) < 0.2
+        ? this.faker.random.words(5)
+        : null;
     vehicle.notes =
       this.faker.datatype.number({ max: 1, min: 0, precision: 0.0000001 }) < 0.2
         ? this.faker.random.words(10)
         : null;
+
     vehicle.organization = this.faker.helpers.arrayElement(
       organizationsFactory.getAll(),
     );
