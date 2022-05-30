52aeeb809706ddc4abefe9151a470cd1be32719e
maonat <romeo.morcia@gmail.com>
2022-05-30 19:46:54 +0200

    Unified vehicles seed in single one

--- backend/src/migrations/1653932337420-CreateVehicles.ts
+++ backend/src/migrations/1653932337420-CreateVehicles.ts
@@ -1,16 +1,16 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class CreateVehicles1653432043000 implements MigrationInterface {
-    name = 'CreateVehicles1653432043000'
+export class CreateVehicles1653932337420 implements MigrationInterface {
+    name = 'CreateVehicles1653932337420'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "vehicle" ADD "organizationId" integer`);
+        await queryRunner.query(`CREATE TABLE "vehicle" ("id" SERIAL NOT NULL, "licensePlate" character varying NOT NULL, "notes" character varying NOT NULL, "organizationId" integer, CONSTRAINT "UQ_a654a0355ae4c5ba31c5f6c8925" UNIQUE ("licensePlate"), CONSTRAINT "PK_187fa17ba39d367e5604b3d1ec9" PRIMARY KEY ("id"))`);
         await queryRunner.query(`ALTER TABLE "vehicle" ADD CONSTRAINT "FK_dd963173bbe67e11aadc6615543" FOREIGN KEY ("organizationId") REFERENCES "organization"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE "vehicle" DROP CONSTRAINT "FK_dd963173bbe67e11aadc6615543"`);
-        await queryRunner.query(`ALTER TABLE "vehicle" DROP COLUMN "organizationId"`);
+        await queryRunner.query(`DROP TABLE "vehicle"`);
     }
 
 }
