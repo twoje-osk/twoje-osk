59030836d14850b870c9eda799bc58d7d4e2c797
maonat <romeo.morcia@gmail.com>
2022-06-17 01:01:06 +0200

    Updated imports and provider for user module

--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -1,7 +1,5 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { CurrentUserService } from 'current-user/current-user.service';
-import { CurrentUserModule } from 'current-user/current-user.module';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { User } from './entities/user.entity';
@@ -9,7 +7,7 @@ import { User } from './entities/user.entity';
 @Module({
   exports: [UsersService],
   controllers: [UsersController],
-  imports: [TypeOrmModule.forFeature([User]), CurrentUserModule],
-  providers: [UsersService, CurrentUserService],
+  imports: [TypeOrmModule.forFeature([User])],
+  providers: [UsersService],
 })
 export class UsersModule {}
