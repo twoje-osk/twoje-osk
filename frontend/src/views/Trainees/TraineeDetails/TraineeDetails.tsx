658ff9a8a7d5640fb9154f336c80f057041b5028
Bartosz Legięć <bartosz@legiec.io>
2022-07-17 18:01:58 +0200

    Add Details, Create, Edit, Activate/Deactivate of Trainee (#38)
    * Rebuild trainee's details
    
    * Add trainee editing
    
    * Add Create new trainee
    
    * Add activate button, add mock deactivation logic
    
    * Rename DeleteModal → ActionModal, add mock activate
    
    * Add proper integration with the BE

--- frontend/src/views/Trainees/TraineeDetails/TraineeDetails.tsx
+++ frontend/src/views/Trainees/TraineeDetails/TraineeDetails.tsx
@@ -3,24 +3,130 @@ import {
   Button,
   Icon,
   Link as MUILink,
+  Stack,
   Toolbar,
   Typography,
 } from '@mui/material';
-import { TraineeFindOneResponseDto } from '@osk/shared';
+import {
+  TraineeFindOneResponseDto,
+  TraineeUpdateRequestDto,
+  TraineeUpdateResponseDto,
+} from '@osk/shared';
 import { parseISO } from 'date-fns';
+import { useCallback } from 'react';
 import { Navigate, useParams, Link } from 'react-router-dom';
 import { Box } from 'reflexbox';
 import useSWR from 'swr';
+import { ActionModal } from '../../../components/ActionModal/ActionModal';
 import { FullPageLoading } from '../../../components/FullPageLoading/FullPageLoading';
 import { GeneralAPIError } from '../../../components/GeneralAPIError/GeneralAPIError';
+import { useCommonSnackbars } from '../../../hooks/useCommonSnackbars/useCommonSnackbars';
+import { useActionModal } from '../../../hooks/useActionModal/useActionModal';
 import { theme } from '../../../theme';
-import { formatLong } from '../../../utils/date';
+import { TraineeForm } from '../TraineeForm/TraineeForm';
+import { TraineeFormData } from '../TraineeForm/TraineeForm.schema';
+import { useMakeRequestWithAuth } from '../../../hooks/useMakeRequestWithAuth/useMakeRequestWithAuth';
 
 export const TraineeDetails = () => {
   const { traineeId } = useParams();
-  const { data, error } = useSWR<TraineeFindOneResponseDto>(
+  const { data, error, mutate } = useSWR<TraineeFindOneResponseDto>(
     traineeId ? `/api/trainees/${traineeId}` : null,
   );
+  const makeRequest = useMakeRequestWithAuth();
+
+  const {
+    isLoading: isDeactivateModalLoading,
+    isOpen: isDeactivateModalOpen,
+    openModal: openDeactivateModal,
+    closeModal: closeDeactivateModal,
+    setLoading: setDeactivateModalLoading,
+  } = useActionModal();
+  const {
+    isLoading: isActivateModalLoading,
+    isOpen: isActivateModalOpen,
+    openModal: openActivateModal,
+    closeModal: closeActivateModal,
+    setLoading: setActivateModalLoading,
+  } = useActionModal();
+  const { showErrorSnackbar, showSuccessSnackbar } = useCommonSnackbars();
+
+  const onDeactivate = useCallback(async () => {
+    setDeactivateModalLoading(true);
+
+    const body: TraineeUpdateRequestDto = {
+      trainee: {
+        user: {
+          isActive: false,
+        },
+      },
+    };
+
+    const response = await makeRequest<
+      TraineeUpdateResponseDto,
+      TraineeUpdateRequestDto
+    >(`/api/trainees/${traineeId}`, 'PATCH', body);
+
+    if (!response.ok) {
+      setDeactivateModalLoading(false);
+      showErrorSnackbar();
+      return;
+    }
+
+    const fullName = `${data?.trainee.user.firstName} ${data?.trainee.user.lastName}`;
+    showSuccessSnackbar(`Kursant ${fullName} został pomyślnie deaktywowany`);
+    await mutate();
+    setDeactivateModalLoading(false);
+    closeDeactivateModal();
+  }, [
+    closeDeactivateModal,
+    data?.trainee.user.firstName,
+    data?.trainee.user.lastName,
+    makeRequest,
+    mutate,
+    setDeactivateModalLoading,
+    showErrorSnackbar,
+    showSuccessSnackbar,
+    traineeId,
+  ]);
+
+  const onActivate = useCallback(async () => {
+    setActivateModalLoading(true);
+
+    const body: TraineeUpdateRequestDto = {
+      trainee: {
+        user: {
+          isActive: true,
+        },
+      },
+    };
+
+    const response = await makeRequest<
+      TraineeUpdateResponseDto,
+      TraineeUpdateRequestDto
+    >(`/api/trainees/${traineeId}`, 'PATCH', body);
+
+    if (!response.ok) {
+      setActivateModalLoading(false);
+      showErrorSnackbar();
+      return;
+    }
+
+    const fullName = `${data?.trainee.user.firstName} ${data?.trainee.user.lastName}`;
+    showSuccessSnackbar(`Kursant ${fullName} został pomyślnie aktywowany`);
+    await mutate();
+    setActivateModalLoading(false);
+    closeActivateModal();
+  }, [
+    closeActivateModal,
+    data?.trainee.user.firstName,
+    data?.trainee.user.lastName,
+    makeRequest,
+    mutate,
+    setActivateModalLoading,
+    showErrorSnackbar,
+    showSuccessSnackbar,
+    traineeId,
+  ]);
 
   if (traineeId === undefined) {
     return <Navigate to="/" replace />;
@@ -35,6 +141,17 @@ export const TraineeDetails = () => {
   }
 
   const { trainee } = data;
+
+  const initialValues: TraineeFormData = {
+    firstName: trainee.user.firstName,
+    lastName: trainee.user.lastName,
+    email: trainee.user.email,
+    phoneNumber: trainee.user.phoneNumber,
+    pesel: trainee.pesel,
+    pkk: trainee.pkk,
+    driversLicenseNumber: trainee.driversLicenseNumber ?? 'Brak',
+    createdAt: parseISO(trainee.user.createdAt),
+  };
 
   return (
     <div>
@@ -59,38 +176,69 @@ export const TraineeDetails = () => {
         </Breadcrumbs>
       </Toolbar>
       <Box as="main" p="16px" pt="0">
-        <Typography variant="h3" component="h1">
-          {trainee.user.firstName} {trainee.user.lastName}
-        </Typography>
-        <Typography variant="h6" component="h2">
-          Telefon
-        </Typography>
-        <div>{trainee.user.phoneNumber}</div>
-        <Typography variant="h6" component="h2">
-          Email
-        </Typography>
-        <div>{trainee.user.email}</div>
-        <Typography variant="h6" component="h2">
-          Numer Dowodu Osobistego
-        </Typography>
-        <div>{trainee.driversLicenseNumber ?? 'Brak'}</div>
-        <Typography variant="h6" component="h2">
-          Data Dołączenia
-        </Typography>
-        <div>{formatLong(parseISO(trainee.user.createdAt))}</div>
-        <Typography variant="h6" component="h2">
-          PESEL
-        </Typography>
-        <div>{trainee.pesel}</div>
-        <Typography variant="h5" component="h2">
-          PKK
-        </Typography>
-        <Typography variant="h6" component="h3">
-          Numer
-        </Typography>
-        <div>{trainee.pkk}</div>
-        <Button variant="contained">Zobacz Raport Postępów</Button>
+        <TraineeForm initialValues={initialValues} disabled>
+          <Stack direction="row" spacing={1}>
+            <Button variant="contained">Zobacz Raport Postępów</Button>
+            <Button
+              variant="outlined"
+              startIcon={<Icon>edit</Icon>}
+              component={Link}
+              to="edytuj"
+            >
+              Edytuj
+            </Button>
+            {!trainee.user.isActive ? (
+              <Button
+                color="success"
+                variant="outlined"
+                startIcon={<Icon>check</Icon>}
+                onClick={openActivateModal}
+              >
+                Aktywuj
+              </Button>
+            ) : (
+              <Button
+                color="error"
+                variant="outlined"
+                startIcon={<Icon>delete</Icon>}
+                onClick={openDeactivateModal}
+              >
+                Deaktywuj
+              </Button>
+            )}
+          </Stack>
+        </TraineeForm>
       </Box>
+      <ActionModal
+        isOpen={isDeactivateModalOpen}
+        isLoading={isDeactivateModalLoading}
+        onClose={closeDeactivateModal}
+        onAction={onDeactivate}
+        id="deactivate-trainee-modal"
+        title="Czy na pewno chcesz deaktywować tego kursanta?"
+        subtitle={
+          <span>
+            Po deaktywowaniu kursant <strong>straci</strong> dostęp do systemu.
+          </span>
+        }
+        actionButtonText="Deaktywuj"
+      />
+      <ActionModal
+        isOpen={isActivateModalOpen}
+        isLoading={isActivateModalLoading}
+        onClose={closeActivateModal}
+        onAction={onActivate}
+        id="activate-trainee-modal"
+        title="Czy na pewno chcesz aktywować tego kursanta?"
+        subtitle={
+          <span>
+            Po aktywowaniu kursant <strong>uzyska</strong> dostęp do systemu.
+          </span>
+        }
+        actionButtonText="Aktywuj"
+        actionButtonColor="success"
+        actionButtonIcon="check"
+      />
     </div>
   );
 };
