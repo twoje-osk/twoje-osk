d6f195378c1dac5a441ed94b019b95432d4bf078
Bartosz Legięć <bartosz@legiec.io>
2022-07-23 16:46:41 +0200

    Remove incorrectly commited files

--- backend/src/admin/admin.imports.ts
+++
@@ -1,7 +0,0 @@
-import { Instructor } from 'instructors/entities/instructor.entity.admin';
-import { User } from 'users/entities/user.entity.admin';
-import { Organization } from 'organizations/entities/organization.entity.admin';
-import { Vehicle } from 'vehicles/entities/vehicle.entity.admin';
-import { Trainee } from 'trainees/entities/trainee.entity.admin';
-
-export const resources = [Instructor, User, Organization, Vehicle, Trainee];
