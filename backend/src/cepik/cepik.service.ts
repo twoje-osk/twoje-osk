786b6070705883601296a8a3003f4e7908c69987
Romeo Morcia <romeo+a.github@morcia.it>
2022-12-11 20:41:57 +0100

    Fixed check on cepik service

--- backend/src/cepik/cepik.service.ts
+++ backend/src/cepik/cepik.service.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import faker from '@faker-js/faker';
 import { Injectable } from '@nestjs/common';
 import { getFailure, getSuccess, Try } from 'types/Try';
@@ -17,7 +16,7 @@ export class CepikService {
     // Adding testing CEPIK_ERROR for testing purposes whenever
     // someone creates a new user with dateOfBirth equal to
     // 1st of January
-    if (dateOfBirth.getDay() === 1 && dateOfBirth.getMonth() === 0) {
+    if (dateOfBirth.getDate() === 1 && dateOfBirth.getMonth() === 0) {
       return getFailure('CEPIK_ERROR');
     }
 
