d5a732143a0024ea6c8920b861ba85fa58576649
Romeo Morcia <romeo+a.github@morcia.it>
2022-12-28 15:39:03 +0100

    Added exports and imports

--- backend/src/course-reports/course-reports.module.ts
+++ backend/src/course-reports/course-reports.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { CourseReportsService } from './course-reports.service';
 import { CourseReportsController } from './course-reports.controller';
+import { CourseReport } from './entities/course-report.entity';
 
 @Module({
+  exports: [CourseReportsService],
   controllers: [CourseReportsController],
+  imports: [TypeOrmModule.forFeature([CourseReport])],
   providers: [CourseReportsService],
 })
 export class CourseReportsModule {}
--- backend/src/report-entry-to-course-report/report-entry-to-course-report.module.ts
+++ backend/src/report-entry-to-course-report/report-entry-to-course-report.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ReportEntryToCourseReportService } from './report-entry-to-course-report.service';
 import { ReportEntryToCourseReportController } from './report-entry-to-course-report.controller';
+import { ReportEntryToCourseReport } from './entities/report-entry-to-course-report.entity';
 
 @Module({
+  exports: [ReportEntryToCourseReportService],
   controllers: [ReportEntryToCourseReportController],
+  imports: [TypeOrmModule.forFeature([ReportEntryToCourseReport])],
   providers: [ReportEntryToCourseReportService],
 })
 export class ReportEntryToCourseReportModule {}
