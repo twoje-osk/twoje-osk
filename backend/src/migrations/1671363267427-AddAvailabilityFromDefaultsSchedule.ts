7672e72ea00012b582e47609541930b0e79428f7
Bartosz Legięć <bartosz@legiec.io>
2023-01-08 12:05:20 +0100

    Run cron migration only if pg_cron extension is available

--- backend/src/migrations/1671363267427-AddAvailabilityFromDefaultsSchedule.ts
+++ backend/src/migrations/1671363267427-AddAvailabilityFromDefaultsSchedule.ts
@@ -1,21 +1,30 @@
 import { MigrationInterface, QueryRunner } from "typeorm"
 
 export class AddAvailabilityFromDefaultsSchedule1671363267427 implements MigrationInterface {
+    private async isCronAvailable(queryRunner: QueryRunner): Promise<boolean> {
+      const [{ count }] = await queryRunner.query(`SELECT COUNT(*) FROM pg_extension WHERE extname = 'pg_cron'`);
+
+      return count > 0;
+    }
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-      await queryRunner.query(
-`
-SELECT cron.schedule(
-  'add_availability_from_defaults_schedule',
-  '0 3 * * 0',
-  $$SELECT "add_availability_from_defaults"()$$
-);
-`
-      );
+        if (await this.isCronAvailable(queryRunner)) {
+            await queryRunner.query(
+  `
+  SELECT cron.schedule(
+    'add_availability_from_defaults_schedule',
+    '0 3 * * 0',
+    $$SELECT "add_availability_from_defaults"()$$
+  );
+  `
+            );
+        }
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-      await queryRunner.query(`SELECT cron.unschedule('add_availability_from_defaults_schedule');`);
+        if (await this.isCronAvailable(queryRunner)) {
+            await queryRunner.query(`SELECT cron.unschedule('add_availability_from_defaults_schedule');`);
+        }
     }
 
 }
