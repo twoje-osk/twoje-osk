1c5b7204135ca21169c581ba2cb40bf0dc059126
vvooycik <maciek.wojcik22@gmail.com>
2023-01-17 22:46:46 +0100

    Fix trigger on deleting one of questions category

--- backend/src/migrations/1673909700830-MockExamsTriggersAndFunctions.ts
+++ backend/src/migrations/1673909700830-MockExamsTriggersAndFunctions.ts
@@ -102,7 +102,7 @@ export class MockExamsTriggersAndFunctions1673909700830 implements MigrationInte
         );
         await queryRunner.query(
             `
-            CREATE OR REPLACE FUNCTION update_amount_on_type_or_points_update() RETURNS trigger AS $$
+            CREATE OR REPLACE FUNCTION update_amount_table_on_type_or_points_update() RETURNS trigger AS $$
                 DECLARE
                     category RECORD;
                 BEGIN
@@ -132,13 +132,42 @@ export class MockExamsTriggersAndFunctions1673909700830 implements MigrationInte
             `
         );
         await queryRunner.query(
+            `
+            CREATE OR REPLACE FUNCTION update_amount_table_on_category_delete() RETURNS trigger AS $$
+            DECLARE
+                question RECORD;
+                category_amount integer;
+            BEGIN
+                FOR question IN
+                    SELECT "points", "typeId"
+                    FROM mock_exam_question
+                    WHERE "id" = OLD."mockExamQuestionId"
+                LOOP
+                    SELECT count(*) INTO category_amount
+                    FROM mock_exam_question_categories_drivers_license_category
+                    WHERE "mockExamQuestionId" = OLD."mockExamQuestionId"
+                    AND "driversLicenseCategoryId" = OLD."driversLicenseCategoryId";
+                    IF category_amount > 0 THEN
+                        UPDATE mock_exam_questions_amount
+                            SET "amount" = "amount" - 1
+                            WHERE "points" = question."points"
+                            AND "typeId" = question."typeId"
+                            AND "categoryId" = OLD."driversLicenseCategoryId";
+                    END IF;
+                END LOOP;
+                RETURN OLD;
+            END
+            $$ LANGUAGE plpgsql;
+            `
+        );
+        await queryRunner.query(
             `       
             CREATE TRIGGER on_type_or_points_update
             AFTER UPDATE
             ON mock_exam_question
             FOR EACH ROW
             WHEN (NEW."points" <> OLD."points" OR NEW."typeId" <> OLD."typeId")
-            EXECUTE PROCEDURE update_amount_on_type_or_points_update();
+            EXECUTE PROCEDURE update_amount_table_on_type_or_points_update();
             `
         );
         await queryRunner.query(
@@ -168,6 +197,15 @@ export class MockExamsTriggersAndFunctions1673909700830 implements MigrationInte
             EXECUTE PROCEDURE update_amount_table_on_questions_category_update();
             `
         );
+        await queryRunner.query(
+            `       
+            CREATE TRIGGER on_questions_category_delete
+            BEFORE DELETE
+            ON mock_exam_question_categories_drivers_license_category
+            FOR EACH ROW
+            EXECUTE PROCEDURE update_amount_table_on_category_delete();
+            `
+        );
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
@@ -175,11 +213,13 @@ export class MockExamsTriggersAndFunctions1673909700830 implements MigrationInte
         await queryRunner.query('drop trigger on_question_insert on mock_exam_question_categories_drivers_license_category cascade;')
         await queryRunner.query('drop trigger on_question_delete on mock_exam_question_categories_drivers_license_category cascade;')
         await queryRunner.query('drop trigger on_questions_category_update on mock_exam_question_categories_drivers_license_category cascade;')
+        await queryRunner.query('drop trigger on_questions_category_delete on mock_exam_question_categories_drivers_license_category cascade;')
         await queryRunner.query('drop trigger on_type_or_points_update on mock_exam_question cascade;')
         await queryRunner.query('drop function update_amount_table_on_question_insert() cascade;')
         await queryRunner.query('drop function update_amount_table_on_question_delete() cascade;')
         await queryRunner.query('drop function update_amount_table_on_questions_category_update() cascade;')
-        await queryRunner.query('drop function update_amount_on_type_or_points_update() cascade;')
+        await queryRunner.query('drop function update_amount_table_on_type_or_points_update() cascade;')
+        await queryRunner.query('drop function update_amount_table_on_category_change() cascade;')
         
     }
 
