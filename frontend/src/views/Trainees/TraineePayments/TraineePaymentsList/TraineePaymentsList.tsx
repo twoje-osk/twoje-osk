5a706db78b5fa58ae0b77e057365624fd4581285
vvooycik <maciek.wojcik22@gmail.com>
2023-05-09 23:56:08 +0200

    Implement filters in admin 'See all trainee's payments' view

--- frontend/src/views/Trainees/TraineePayments/TraineePaymentsList/TraineePaymentsList.tsx
+++ frontend/src/views/Trainees/TraineePayments/TraineePaymentsList/TraineePaymentsList.tsx
@@ -4,36 +4,91 @@ import {
   Stack,
   Button,
   Icon,
-  TableContainer,
-  Table,
-  TableHead,
-  TableRow,
   TableCell,
-  TableBody,
   Breadcrumbs,
   Link as MUILink,
+  Divider,
 } from '@mui/material';
 import {
   PaymentFindAllByTraineeResponseDto,
+  PaymentFindAllQueryDto,
   TraineeFindOneResponseDto,
 } from '@osk/shared';
-import { parseISO } from 'date-fns';
+
+import { endOfDay, formatISO, parseISO } from 'date-fns';
 import { Link, useNavigate, useParams } from 'react-router-dom';
 import { Flex } from 'reflexbox';
 import useSWR from 'swr';
+import { useMemo, useState } from 'react';
+import { Table } from '../../../../components/Table/Table';
 import { FullPageLoading } from '../../../../components/FullPageLoading/FullPageLoading';
 import { GeneralAPIError } from '../../../../components/GeneralAPIError/GeneralAPIError';
 import { formatCurrency } from '../../../../utils/currency';
-import { formatLong } from '../../../../utils/date';
+import { formatDatesRange, formatLong } from '../../../../utils/date';
 import { theme } from '../../../../theme';
 import { AmountTableCell } from './TraineePaymentsList.styled';
+import { usePagination } from '../../../../hooks/usePagination/usePagination';
+import { useSort } from '../../../../hooks/useSort/useSort';
+import { addQueryParams } from '../../../../utils/addQueryParams';
+import { TableFilters } from '../../../../components/Table/TableFilters';
+import { TextFilter } from '../../../../components/Table/Filters/TextFilter/TextFilter';
+import { DateFilter } from '../../../../components/Table/Filters/DateFilter/DateFilter';
+import { IntegerRangeFilter } from '../../../../components/Table/Filters/IntegerRangeFilter/IntegerRangeFilter';
 
+const MIN_AMOUNT = 0;
+const MAX_AMOUNT = 10000;
 export const TraineePaymentsList = () => {
   const { traineeId } = useParams();
   const navigate = useNavigate();
-  const { data, error } = useSWR<PaymentFindAllByTraineeResponseDto>(
-    traineeId ? `/api/payments/trainees/${traineeId}` : null,
+  const { rowsPerPage, currentPage, onPageChange, onRowsPerPageChange } =
+    usePagination();
+  const {
+    sortOrder,
+    sortedBy,
+    getCellSortDirection,
+    getLabelIsActive,
+    getLabelSortDirection,
+    onSortClick,
+  } = useSort<Required<PaymentFindAllQueryDto>['sortBy']>('date', 'asc');
+  const { note, amount, date } = useFilters();
+  const [openedFilter, setOpenedFilter] = useState<null | string>(null);
+
+  const apiUrl = useMemo(
+    () =>
+      addQueryParams<PaymentFindAllQueryDto>(
+        `/api/payments/trainees/${traineeId}`,
+        {
+          page: currentPage,
+          pageSize: rowsPerPage,
+          sortOrder,
+          sortBy: sortedBy,
+          filters: {
+            dateFrom:
+              date.value.from !== undefined
+                ? formatISO(date.value.from)
+                : undefined,
+            dateTo:
+              date.value.to !== undefined
+                ? formatISO(endOfDay(date.value.to))
+                : undefined,
+            note: note.value,
+            amountFrom: amount.value.from,
+            amountTo: amount.value.to,
+          },
+        },
+      ),
+    [
+      traineeId,
+      currentPage,
+      rowsPerPage,
+      sortOrder,
+      sortedBy,
+      note.value,
+      date.value,
+      amount.value,
+    ],
   );
+  const { data, error } = useSWR<PaymentFindAllByTraineeResponseDto>(apiUrl);
   const { data: traineeData, error: traineeError } =
     useSWR<TraineeFindOneResponseDto>(
       traineeId ? `/api/trainees/${traineeId}` : null,
@@ -43,11 +98,12 @@ export const TraineePaymentsList = () => {
     return <GeneralAPIError />;
   }
 
-  if (data === undefined || traineeData === undefined) {
+  if (traineeData === undefined) {
     return <FullPageLoading />;
   }
 
-  const rows = data.payments;
+  const rows = data?.payments;
+  const totalRows = data?.total ?? 0;
   const { trainee } = traineeData;
 
   return (
@@ -91,33 +147,133 @@ export const TraineePaymentsList = () => {
           </Button>
         </Stack>
       </Toolbar>
-      <TableContainer sx={{ flex: '1 1', overflow: 'auto' }}>
-        <Table aria-label="Lista Płatności Kursanta" stickyHeader>
-          <TableHead>
-            <TableRow>
-              <TableCell>Data</TableCell>
-              <TableCell>Uwagi</TableCell>
-              <TableCell align="right">Kwota</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {rows.map((row) => (
-              <TableRow
-                hover
-                key={row.id}
-                onClick={() => navigate(`./${row.id}`)}
-                sx={{ cursor: 'pointer' }}
-              >
-                <TableCell>{formatLong(parseISO(row.date))}</TableCell>
-                <TableCell>{row.note.trim() || 'Brak'}</TableCell>
-                <AmountTableCell align="right">
-                  {formatCurrency(row.amount)}
-                </AmountTableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
+      <Divider />
+      <TableFilters
+        openedFilter={openedFilter}
+        setOpenedFilter={setOpenedFilter}
+        filters={[
+          {
+            id: 'note',
+            label: 'Uwagi',
+            isActive: note.value !== undefined,
+            activeLabel: note.value,
+            clearFilter: () => note.set(undefined),
+            renderFilter: ({ isOpen, toggle }) => (
+              <TextFilter
+                value={note.value}
+                setValue={note.set}
+                label="Uwagi"
+                isOpen={isOpen}
+                toggleOpen={toggle}
+              />
+            ),
+          },
+          {
+            id: 'date',
+            label: 'Data',
+            isActive:
+              date.value.from !== undefined || date.value.to !== undefined,
+            activeLabel: formatDatesRange(date.value.from, date.value.to),
+            clearFilter: () => date.set(undefined, undefined),
+            renderFilter: ({ toggle }) => (
+              <DateFilter
+                valueFrom={date.value.from}
+                valueTo={date.value.to}
+                setValue={date.set}
+                toggleOpen={toggle}
+              />
+            ),
+          },
+          {
+            id: 'amount',
+            label: 'Kwota',
+            isActive: true,
+            activeLabel: `${amount.value.from}zł - ${amount.value.to}zł`,
+            clearFilter: () => amount.set({ from: MIN_AMOUNT, to: MAX_AMOUNT }),
+            renderFilter: () => (
+              <IntegerRangeFilter
+                label="Kwota"
+                setValue={amount.set}
+                min={MIN_AMOUNT}
+                max={MAX_AMOUNT}
+              />
+            ),
+          },
+        ]}
+      />
+      <Table
+        ariaLabel="Lista Płatności Kursanta"
+        totalRows={totalRows}
+        rowsPerPage={rowsPerPage}
+        currentPage={currentPage}
+        onPageChange={onPageChange}
+        onRowsPerPageChange={onRowsPerPageChange}
+        getCellSortDirection={getCellSortDirection}
+        getLabelIsActive={getLabelIsActive}
+        getLabelSortDirection={getLabelSortDirection}
+        onSortClick={onSortClick}
+        columns={[
+          {
+            id: 'date',
+            name: 'Data',
+            width: '20%',
+            sortable: true,
+            openFilter: () => setOpenedFilter('date'),
+          },
+          {
+            id: 'note',
+            name: 'Uwagi',
+            width: '60%',
+            openFilter: () => setOpenedFilter('note'),
+          },
+          {
+            id: 'amount',
+            name: 'Kwota',
+            width: '20%',
+            sortable: true,
+            openFilter: () => setOpenedFilter('amount'),
+          },
+        ]}
+        rows={rows}
+        onRowClick={(row) => navigate(`./${row.id}`)}
+        renderRowCells={(row) => {
+          return (
+            <>
+              <TableCell>{formatLong(parseISO(row.date))}</TableCell>
+              <TableCell>{row.note.trim() || 'Brak'}</TableCell>
+              <AmountTableCell align="right">
+                {formatCurrency(row.amount)}
+              </AmountTableCell>
+            </>
+          );
+        }}
+      />
     </Flex>
+  );
+};
+
+const useFilters = () => {
+  const [note, setNote] = useState<string | undefined>();
+  const [date, setDate] = useState<{
+    from: Date | undefined;
+    to: Date | undefined;
+  }>({ from: undefined, to: undefined });
+  const [amount, setAmount] = useState<{
+    from: number;
+    to: number;
+  }>({ from: MIN_AMOUNT, to: MAX_AMOUNT });
+
+  return useMemo(
+    () => ({
+      note: { set: setNote, value: note },
+      amount: { set: setAmount, value: amount },
+      date: {
+        set: (from: Date | undefined, to: Date | undefined) => {
+          setDate({ from, to });
+        },
+        value: date,
+      },
+    }),
+    [note, amount, date],
   );
 };
