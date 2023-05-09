c27bf745ad71ba53b12a850e6caeb765348bcf2a
Romeo Morcia <56087756+maonat@users.noreply.github.com>
2023-05-09 11:40:01 +0200

    BE Filter instructors on getAll() service (#94)
    * Added filtering on Instructors
    
    * Removal of array filtering on qualifications

---
+++ backend/src/instructors/instructors.utils.ts
@@ -0,0 +1,13 @@
+export const INSTRUCTOR_USER_SORT_FIELDS = [
+  'firstName',
+  'lastName',
+  'isActive',
+] as const;
+export type InstructorUserSortField =
+  typeof INSTRUCTOR_USER_SORT_FIELDS[number];
+
+export function isInstructorUserSortField(
+  text: string,
+): text is InstructorUserSortField {
+  return (INSTRUCTOR_USER_SORT_FIELDS as readonly string[]).includes(text);
+}
