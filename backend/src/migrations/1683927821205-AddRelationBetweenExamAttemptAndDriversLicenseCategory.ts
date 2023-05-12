e090d8997b74d7f942a908d662bbb52eb4215c6d
vvooycik <maciek.wojcik22@gmail.com>
2023-05-12 23:58:36 +0200

    Fix migration

--- backend/src/migrations/1683927821205-AddRelationBetweenExamAttemptAndDriversLicenseCategory.ts
+++ backend/src/migrations/1683927821205-AddRelationBetweenExamAttemptAndDriversLicenseCategory.ts
@@ -19,7 +19,7 @@ export class AddRelationBetweenExamAttemptAndDriversLicenseCategory1683927821205
         return id;
     }
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "mock_exam_attempt" ADD "categoryId" integer NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "mock_exam_attempt" ADD "categoryId" integer`);
         await queryRunner.query(`ALTER TABLE "mock_exam_attempt" ADD CONSTRAINT "FK_1186bf72ef8a012bdeb7b7c8b04" FOREIGN KEY ("categoryId") REFERENCES "drivers_license_category"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
         const doAttemptsExist = await this.checkIfMockExamAttemptsExist(queryRunner)
 
