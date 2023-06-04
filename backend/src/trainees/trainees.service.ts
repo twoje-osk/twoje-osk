cee229d52d69531bfea563fc7521436bbef2f1a4
vvooycik <maciek.wojcik22@gmail.com>
2023-06-04 22:45:26 +0200

    Add escaping '%' char in trainees service

--- backend/src/trainees/trainees.service.ts
+++ backend/src/trainees/trainees.service.ts
@@ -78,7 +78,12 @@ export class TraineesService {
             () => {
               return `"firstName" || ' ' || "lastName" || ' ' || "phoneNumber" ILIKE :searchedPhrase`;
             },
-            { searchedPhrase: `%${filterArguments.searchedPhrase}%` },
+            {
+              searchedPhrase: `%${filterArguments.searchedPhrase.replace(
+                /[\\%_]/g,
+                (match) => `\\${match}`,
+              )}%`,
+            },
           )
         : undefined;
 
